11061e75a21b5d2ea8fdae8a01506c0416db8aed
morrillo <gustavo.orrillo@moldeo.coop>
2015-08-27 11:27:58 -0300

    First commit

---
+++ __openerp__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'CRM - Information on customers',
+    'category': 'Sales',
+    'version': '1.1',
+    'depends': ['crm', 'base','sale','account'],
+    'data': [
+        'partner_view.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [],
+    'installable': True,
+}
